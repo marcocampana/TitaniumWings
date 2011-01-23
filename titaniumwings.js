bc69e5ebf75d57429891324ff14de7fce5627277
Marco Campana <m.campana@gmail.com>
2011-01-23 13:41:46 +0000

    Started work on automatic include of app files. Including controllers files for now

--- titaniumwings.js
+++ titaniumwings.js
@@ -1,6 +1,14 @@
-titaniumWingsRoot = 'vendor/titaniumwings'
+WINGS_ROOT = 'vendor/titaniumwings';
 
-wingsFiles = [
-  titaniumWingsRoot + '/wingsresource/wings_controller.js'
-];
-wingsFiles.each(function(file) { Ti.include(file) });
+Ti.include(WINGS_ROOT + '/wingsresource/wings_controller.js');
+
+// TODO extract this into a method and include models and lib files
+var resourcesDir = Titanium.Filesystem.getResourcesDirectory();
+var dir = Titanium.Filesystem.getFile(resourcesDir + '/app/controllers');
+var dir_files = dir.getDirectoryListing();
+
+for (var i=1;i<dir_files.length;i++){
+  if (dir_files[i].toString().match(/.*_controller\.js/)) {
+    Ti.include( 'app/controllers/' + dir_files[i]);
+  }
+}
